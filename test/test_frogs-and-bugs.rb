c93e71e19730c013dd4f93c0e4fa1e5805f3bf7a
Renée De Voursney <renee@nird.us>
2012-10-15 12:34:24 -0700

    really add the devise and carrierwave examples

--- test/test_frogs-and-bugs.rb
+++
@@ -1,7 +0,0 @@
-require 'helper'
-
-class TestFrogsAndBugs < Test::Unit::TestCase
-  should "reccomend a dinner selection for my frog" do
-    assert_respond_to(FrogsAndBugs, :recommend_dinner_selection)
-  end
-end
